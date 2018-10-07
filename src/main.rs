f73a0861266d19a16f666e84de95cf888d565278
Seiichi KONDO <seikichi@kmc.gr.jp>
2018-10-07 16:08:46 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,257 @@
+fn main() {
+    let old_html = "<p>Hello, world!</p><div>AAA</div>";
+    let new_html = "<p>Hello, seikichi!</p><p>HAHAHA</p>";
+
+    let old_words = convert_html_to_list_of_words(old_html);
+    let new_words = convert_html_to_list_of_words(new_html);
+
+    println!("old = {:?}", old_words);
+    println!("new = {:?}", new_words);
+
+    let ses = diff(&old_words, &new_words);
+
+    // for e in &ses {
+    //     match e {
+    //         Edit::Common { old, new: _ } => println!(" {}", old_words[*old]),
+    //         Edit::Add { new } => println!("+{}", new_words[*new]),
+    //         Edit::Delete { old } => println!("-{}", old_words[*old]),
+    //     }
+    // }
+
+    let contents = perform(&old_words, &new_words, &ses);
+    for c in &contents {
+        print!("{}", c);
+    }
+    println!("");
+}
+
+fn perform<'a>(old_words: &[&'a str], new_words: &[&'a str], ses: &[Edit]) -> Vec<&'a str> {
+    let mut contents = vec![];
+    for edit in ses {
+        match edit {
+            Edit::Common { old, new: _ } => {
+                contents.push(old_words[*old]);
+            }
+            Edit::Add { new } => {
+                contents.push("<ins>");
+                contents.push(new_words[*new]);
+                contents.push("</ins>");
+            }
+            Edit::Delete { old } => {
+                contents.push("<del>");
+                contents.push(old_words[*old]);
+                contents.push("</del>");
+            }
+        }
+    }
+    contents
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+enum Edit {
+    Add { new: usize },
+    Delete { old: usize },
+    Common { new: usize, old: usize },
+}
+
+struct PointWithPrev {
+    x: usize,
+    y: usize,
+    prev: isize,
+}
+
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+fn diff<T: Eq>(a: &[T], b: &[T]) -> Vec<Edit> {
+    let m = a.len();
+    let n = b.len();
+    if m > n {
+        return diff(b, a);
+    }
+    let offset = (m + 1) as isize;
+    let delta = (n - m) as isize;
+    let delta_offset = (delta + offset) as usize;
+
+    // for SES
+    let mut ids = vec![-1; m + n + 3];
+    let mut points = vec![];
+    {
+        let mut snake = |k: isize, fp1: isize, fp2: isize| -> isize {
+            let fp = std::cmp::max(fp1, fp2);
+            let mut y = fp as usize;
+            let mut x = (fp - k) as usize;
+            while x < m && y < n && a[x] == b[y] {
+                x += 1;
+                y += 1;
+            }
+            // SES
+            let ko = (k + offset) as usize;
+            // NOTE: modify >= to > to change delete/insert orders.
+            let prev = if fp1 >= fp2 { ids[ko - 1] } else { ids[ko + 1] };
+            ids[ko] = points.len() as isize;
+            points.push(PointWithPrev { x, y, prev });
+            y as isize
+        };
+
+        let mut fp = vec![-1; m + n + 3];
+        let mut p = -1;
+        loop {
+            p += 1;
+            for k in -p..delta {
+                let ko = (k + offset) as usize;
+                fp[ko] = snake(k, fp[ko - 1] + 1, fp[ko + 1]);
+            }
+            for k in ((delta + 1)..=(delta + p)).rev() {
+                let ko = (k + offset) as usize;
+                fp[ko] = snake(k, fp[ko - 1] + 1, fp[ko + 1]);
+            }
+            fp[delta_offset] = snake(delta, fp[delta_offset - 1] + 1, fp[delta_offset + 1]);
+
+            if fp[delta_offset] >= (n as isize) {
+                break;
+            }
+        }
+    }
+
+    let mut route: Vec<Point> = vec![];
+    let mut prev = ids[(delta + offset) as usize];
+    while prev != -1 {
+        let p = &points[prev as usize];
+        route.push(Point { x: p.x, y: p.y });
+        prev = p.prev;
+    }
+    let mut px = 0;
+    let mut py = 0;
+    let mut ses = vec![];
+    for p in route.iter().rev() {
+        while px < p.x || py < p.y {
+            // compare (p.y - p.x) and (py - px)
+            if p.y + px > p.x + py {
+                ses.push(Edit::Add { new: py });
+                py += 1;
+            } else if p.y + px < p.x + py {
+                ses.push(Edit::Delete { old: px });
+                px += 1;
+            } else {
+                ses.push(Edit::Common { old: px, new: py });
+                px += 1;
+                py += 1;
+            }
+        }
+    }
+    ses
+}
+
+fn is_end_of_tag(c: char) -> bool {
+    c == '>'
+}
+
+fn is_start_of_tag(c: char) -> bool {
+    c == '<'
+}
+
+fn is_whitespace(c: char) -> bool {
+    c.is_whitespace()
+}
+
+fn is_in_word(c: char) -> bool {
+    c.is_alphanumeric() || c == '#' || c == '@'
+}
+
+fn convert_html_to_list_of_words(s: &str) -> Vec<&str> {
+    enum Mode {
+        Char,
+        Tag,
+        Whitespace,
+    }
+    let mut words = vec![];
+    let mut start = 0;
+    let mut mode = Mode::Char;
+
+    for (i, c) in s.chars().enumerate() {
+        match mode {
+            Mode::Char if is_start_of_tag(c) => {
+                if start != i {
+                    words.push(&s[start..i]);
+                }
+                start = i;
+                mode = Mode::Tag;
+            }
+            Mode::Char if is_whitespace(c) => {
+                if start != i {
+                    words.push(&s[start..i]);
+                }
+                start = i;
+                mode = Mode::Whitespace;
+            }
+            Mode::Char if is_in_word(c) => { /* continue */ }
+            Mode::Char => {
+                if start != i {
+                    words.push(&s[start..i]);
+                }
+                start = i;
+            }
+            Mode::Tag if is_end_of_tag(c) => {
+                words.push(&s[start..=i]);
+                start = i + 1;
+                mode = Mode::Char;
+            }
+            Mode::Tag => { /* continue */ }
+            Mode::Whitespace if is_start_of_tag(c) => {
+                if start != i {
+                    words.push(&s[start..i]);
+                }
+                start = i;
+                mode = Mode::Tag;
+            }
+            Mode::Whitespace if is_whitespace(c) => { /* continue */ }
+            Mode::Whitespace => {
+                if start != i {
+                    words.push(&s[start..i]);
+                }
+                start = i;
+                mode = Mode::Char;
+            }
+        }
+    }
+
+    if start < s.len() {
+        words.push(&s[start..]);
+    }
+
+    words
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_convert_html() {
+        let actual = convert_html_to_list_of_words("<p>Hello, world</p>");
+        let expected = vec!["<p>", "Hello", ",", " ", "world", "</p>"];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_diff() {
+        let actual = diff(
+            &vec!["み", "ん", "か", "ん", "じ", "ん"],
+            &vec!["み", "か", "ん", "せ", "い", "じ", "ん"],
+        );
+        let expected = vec![
+            Edit::Common { old: 0, new: 0 },
+            Edit::Delete { old: 1 },
+            Edit::Common { old: 2, new: 1 },
+            Edit::Common { old: 3, new: 2 },
+            Edit::Add { new: 3 },
+            Edit::Add { new: 4 },
+            Edit::Common { old: 4, new: 5 },
+            Edit::Common { old: 5, new: 6 },
+        ];
+        assert_eq!(actual, expected);
+    }
+}
